13f19661959c2224ed8e6e3262f38bb89488c195
PrettyChillDev <81452894+ChillCordDev@users.noreply.github.com>
2021-08-01 16:36:11 -0600

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { ReactComponent as Topgg } from "../assets/logo.svg";
+import { ReactComponent as IBL } from "../assets/logo.svg";
 
 function Logo() {
   return (
     <div style={{ display: "flex", alignItems: "center" }}>
-      <Topgg />
+      <IBL />
       <p style={{ margin: "0 0 0 15px" }} className="site-title">
-        Top.gg Documentation
+        Infinity Docs
       </p>
     </div>
   );
