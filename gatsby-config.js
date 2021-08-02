3da6fae9781204131a46b10670fbc2228e9b9dc9
PrettyChillDev <81452894+ChillCordDev@users.noreply.github.com>
2021-08-01 21:26:54 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         //   1: "version-1",
         // },
         sidebarCategories: {
-           api: filesOf("api")
+           api: filesOf("api"),
         },
       },
     },
